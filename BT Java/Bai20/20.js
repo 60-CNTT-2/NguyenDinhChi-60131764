cf0a5e37bee3edb720bef739c4039b5b30ee8608
Nguyễn Đình Chí <62933861+nguyendinhchi@users.noreply.github.com>
2020-07-17 10:21:16 +0700

    bai20

---
+++ BT Java/Bai20/20.js
@@ -0,0 +1,10 @@
+function new2()
+{
+    var a2 = document.getElementById('a2');
+    a2.onclick = alert(2);
+}
+function new3()
+{
+    var a3 = document.getElementById('a3');
+    a3.onclick = alert(3);
+}
